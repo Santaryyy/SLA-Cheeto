c8eef5ac6f4e5d1116b7c7ffd4c7da9b66d816be
Santaryyy <141366580+Santaryyy@users.noreply.github.com>
2024-04-06 13:40:33 +0200

    Update NoCooldown.cpp
    After a round, when No Cooldown was activated, error 14 (The character basic information is unavailable.) was displayed. This update fixes that

--- src/game/cheat/features/NoCooldown.cpp
+++ src/game/cheat/features/NoCooldown.cpp
@@ -3,35 +3,35 @@
 
 namespace Cheat::Features
 {
-	NoCooldown::NoCooldown()
-	{
-		HookManager::install(app::KAAIFMKPKAG_IOFMGMJCCFO, KAAIFMKPKAG_IOFMGMJCCFO_Hook);
-		HookManager::install(app::JLLMICINIDA_LFCGJLAFFOM, JLLMICINIDA_LFCGJLAFFOM_Hook);
-	}
+    NoCooldown::NoCooldown()
+    {
+        HookManager::install(app::KAAIFMKPKAG_IOFMGMJCCFO, KAAIFMKPKAG_IOFMGMJCCFO_Hook);
+        HookManager::install(app::JLLMICINIDA_LFCGJLAFFOM, JLLMICINIDA_LFCGJLAFFOM_Hook);
+    }
 
-	void NoCooldown::JLLMICINIDA_LFCGJLAFFOM_Hook(app::JLLMICINIDA* __this, MethodInfo* method)
-	{
-		auto& vars = Vars::GetInstance();
+    void NoCooldown::JLLMICINIDA_LFCGJLAFFOM_Hook(app::JLLMICINIDA* __this, MethodInfo* method)
+    {
+        auto& vars = Vars::GetInstance();
 
-		if (vars.NoCooldownShadow.value() && __this)
-		{
-			__this->fields.DGPHADJBCDJ = 0.0f;
-			__this->fields.KKEDBBAMIJL = 1;
-		}
+        if (vars.NoCooldownShadow.value() && __this)
+        {
+            __this->fields.DGPHADJBCDJ = 0.1f; // fixed error 144
+            __this->fields.KKEDBBAMIJL = 1;
+        }
 
-		return CALL_ORIGIN(JLLMICINIDA_LFCGJLAFFOM_Hook, __this, method);
-	}
+        return CALL_ORIGIN(JLLMICINIDA_LFCGJLAFFOM_Hook, __this, method);
+    }
 
-	void* NoCooldown::KAAIFMKPKAG_IOFMGMJCCFO_Hook(app::KAAIFMKPKAG* __this, bool DEJNILEHENL, MethodInfo* method)
-	{
-		auto& vars = Vars::GetInstance();
-		
-		if (vars.NoCooldown.value())
-		{
-			__this->fields.HPHOOEPCBOJ = 0;
-			__this->fields.JCJJDJGOBJL = 0;
-		}
+    void* NoCooldown::KAAIFMKPKAG_IOFMGMJCCFO_Hook(app::KAAIFMKPKAG* __this, bool DEJNILEHENL, MethodInfo* method)
+    {
+        auto& vars = Vars::GetInstance();
 
-		return CALL_ORIGIN(KAAIFMKPKAG_IOFMGMJCCFO_Hook, __this, DEJNILEHENL, method);
-	}
+        if (vars.NoCooldown.value())
+        {
+            __this->fields.HPHOOEPCBOJ = 0.1f; // fixed error 144
+            __this->fields.JCJJDJGOBJL = 0;
+        }
+
+        return CALL_ORIGIN(KAAIFMKPKAG_IOFMGMJCCFO_Hook, __this, DEJNILEHENL, method);
+    }
 }
